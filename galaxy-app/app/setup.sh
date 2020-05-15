ad52445f934b6d63ee78bebbd5adf1e4b0c2eedb
Eric Lopes <eric@quave.com.br>
2020-05-15 15:37:29 -0400

    Remove puppeteer user

--- galaxy-app/app/setup.sh
+++ galaxy-app/app/setup.sh
@@ -25,18 +25,6 @@ pushd programs/server
 # Pass --unsafe-perm in order to still run scripts even though we run as root.
 npm install --unsafe-perm
 
-# Install puppeteer so it's available in the container.
-RUN npm i puppeteer \
-    # Add user so we don't need --no-sandbox.
-    # same layer as npm install to keep re-chowned files from using up several hundred MBs more space
-    && groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
-    && mkdir -p /home/pptruser/Downloads \
-    && chown -R pptruser:pptruser /home/pptruser \
-    && chown -R pptruser:pptruser /node_modules
-
-# Run everything after as non-privileged user.
-USER pptruser
-
 # Run custom setup.sh in programs/server if provided, as ROOT.
 if [ -x ./setup.sh ]; then
   bash ./setup.sh
